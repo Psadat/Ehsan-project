b59d03c9ad7ccf5baead89edebe667900922b70f
Psadat <parisasr23@gmail.com>
2021-09-04 10:42:22 +0430

    Exercise01

--- Project01.js
+++ Project01.js
@@ -1,1 +1,22 @@
 // This is my first Project
+
+const myArr = [7, 5, 10, 5, "parisa", 5, 7, 3, 5]
+
+//let number = 5;
+
+
+function searchArr(arr,number){
+    let flag = 0;
+    for( let x of myArr){
+        if( x === number){
+            flag++
+        }
+    }
+    return flag;
+}
+
+
+
+
+
+console.log(searchArr(myArr,7));
